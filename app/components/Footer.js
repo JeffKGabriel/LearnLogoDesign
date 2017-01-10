d048f0f60fa69565530b2fbe87e5aa0c77d45d9d
root <root@oel72.federated.fds>
2017-01-10 15:57:17 -0500

    .

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,15 +1,11 @@
 var React = require('react');
-//import { connect } from 'react-redux'
-//var ReactRouter = require('react-router');
-//import { bindActionCreators } from 'redux'
-//var Link = ReactRouter.Link;
 
 const Footer = React.createClass({
 
   render: function(){
 
     return(
-      <div className="col-sm-12 footer">
+      <div className="col-xs-12 footer">
         Footer
       </div>
     )
--- app/components/Home.js
+++ app/components/Home.js
@@ -1,13 +1,55 @@
 var React = require('react');
+import { connect } from 'react-redux'
+//var ReactRouter = require('react-router');
+import { bindActionCreators } from 'redux'
+//var Link = ReactRouter.Link;
+
+const increaseAction = { type: 'increase', amount: 1 }
+const decreaseAction = { type: 'decrease', amount: 1 }
+
 
 const Home = React.createClass({
   render: function(){
+    const { value, onIncreaseClick, onDecreaseClick } = this.props
     return(
       <div>
-        Home
+        <div className="jumbotron text-center">
+          <h1>{value}</h1>
+          <button
+            className="btn btn-default"
+            onClick={onIncreaseClick}
+          >
+            Increase
+          </button>
+          <button
+            className="btn btn-default"
+            onClick={onDecreaseClick}
+          >
+            Decrease
+          </button>
+        </div>
       </div>
     )
   },
 });
 
-module.exports = Home;
+// Map Redux state to component props
+function mapStateToProps(state) {
+  return {
+    value: state.counter.count
+  }
+}
+
+// Map Redux actions to component props
+
+function mapDispatchToProps(dispatch) {
+  return {
+    onIncreaseClick: () => dispatch(increaseAction),
+    onDecreaseClick: () => dispatch(decreaseAction)
+  }
+}
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Home)
